d9c3e922074854bd20089656ba807aad5636a9c2
lucho <lucho@LuisPenagosG>
2018-04-04 21:32:09 -0500

    Cambio de código

--- src/prueba_1/Prueba_1.java
+++ src/prueba_1/Prueba_1.java
@@ -5,17 +5,14 @@
  */
 package prueba_1;
 
-/**
- *
- * @author lucho
- */
+
 public class Prueba_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+    int numero;
+    numero = 1;
+    System.out.println("El número es: " + 1);
     }
     
 }
